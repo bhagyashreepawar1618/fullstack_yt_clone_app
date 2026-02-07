df1e2260e34dd000e5c6c4223f826ae6a5ce3827
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-08 00:03:46 +0530

    implement asyncHandler utility for handling asynchronous route handlers

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//const asyncHandler = () => {};
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (e) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: er.message,
+//     });
+//   }
+// };
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+export default asyncHandler;
