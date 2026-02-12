4ee803622769a93b541eedcb3dccb68d8f06ac12
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-13 01:03:52 +0530

    Layout setup

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <>
+      <p className="bg-red-900 text-white">this is footer</p>
+    </>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <>
+      <p className="bg-pink-200 text-white">this is my header</p>
+    </>
+  );
+}
+
+export default Header;
