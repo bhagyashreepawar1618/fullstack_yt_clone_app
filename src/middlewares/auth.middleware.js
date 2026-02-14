519e5453d4293214e4a6939891121fa49259df7c
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-02-14 23:52:20 +0530

    auth middleware taking data from cookies

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,40 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiErrors.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  //access token from cookies
+  //we've set cookies at the time of login
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    //if token is not there throw an error
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    //if token is present
+    //verify compare accesstoken secret present in cookie and in our server
+    //and returns info that we've set at the time of token creation
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    //now we have the data (_id) so we can get instance
+    const user = await User.findById(decodedToken?._id).select(
+      " -password -refreshToken"
+    );
+
+    //if user not found
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    //   if user is present
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid AccessToken");
+  }
+});
